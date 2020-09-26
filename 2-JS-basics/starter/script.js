95c0317ea7d8ed4310a0d91d8b502e298844d17d
thetarnav <gthetarnav@gmail.com>
2020-09-26 16:48:44 +0200

    Solution to challange #2

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,18 +1,45 @@
-const bmi = (m, h) => m / h ** 2
-
-class BMI {
-	constructor(height, mass) {
-		this.height = height
-		this.mass = mass
-		this._bmi = bmi(mass, height)
+class Score {
+	constructor(...scores) {
+		this.scores = scores
 	}
 
-	get bmi() {
-		return this._bmi
+	changeScore(MatchIndex, score) {
+		if (
+			typeof MatchIndex !== 'number' ||
+			MatchIndex >= this.scores.length ||
+			typeof score !== 'number'
+		)
+			return
+
+		this.scores[MatchIndex] = score
+	}
+
+	get score() {
+		const total = this.scores.reduce((n, total) => total + n)
+		return total
+	}
+
+	get average() {
+		const average = this.score / this.scores.length
+		return average
 	}
 }
 
-const markBMI = new BMI(1.79, 70)
-const johnBMI = new BMI(1.83, 80)
+const john = new Score(89, 120, 103)
+const mike = new Score(116, 94, 123)
+const mary = new Score(97, 134, 105)
 
-console.log(`Is Marks's BMI higher than John's? ${markBMI.bmi > johnBMI.bmi}`)
+john.changeScore(1, 130)
+
+console.log(`Mike's average score: ` + mike.average)
+console.log(`John's average score: ` + john.average)
+console.log(`Mary's average score: ` + mary.average)
+
+let string = `And the winner is: `
+
+if (john.average > mary.average && john.average > mike.average) string += 'John'
+else if (mary.average > john.average && mary.average > mike.average)
+	string += 'Mary'
+else string += 'Mike'
+
+console.log(string)
