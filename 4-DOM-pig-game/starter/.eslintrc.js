3f7dd6766063832b740e6a0375ce26c8010cfc43
thetarnav <gthetarnav@gmail.com>
2020-09-29 18:28:13 +0200

    Updated styles to match new dice game

---
+++ 4-DOM-pig-game/starter/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: 'eslint:recommended',
+	parserOptions: {
+		ecmaVersion: 12,
+		sourceType: 'module',
+	},
+	rules: {
+		indent: ['warn', 'tab'],
+		'linebreak-style': ['warn', 'windows'],
+		quotes: ['warn', 'single'],
+		semi: ['warn', 'never'],
+	},
+}
